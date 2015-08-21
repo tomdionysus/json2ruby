deece85f84d6c31849571277da45674587d724b4
Tom Cully <tom.cully@loyalty.co.nz>
2015-08-21 14:40:34 +1200

    Reenable coveralls #2

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,9 +2,6 @@
 warn_level = $VERBOSE
 $VERBOSE = nil
 
-require 'coveralls'
-Coveralls.wear!
-
 require 'simplecov'
 require 'simplecov-rcov'
 
@@ -12,5 +9,8 @@ SimpleCov.formatter = SimpleCov::Formatter::RcovFormatter
 SimpleCov.start do
   add_filter '/spec/'
 end
+
+require 'coveralls'
+Coveralls.wear!
 
 require 'json2ruby'
